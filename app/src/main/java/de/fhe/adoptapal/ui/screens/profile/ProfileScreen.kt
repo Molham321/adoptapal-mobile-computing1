db62758d62e1066cfba73f80392d470201e8fce5
Molham Al khodari <alkhodari@drherwig.de>
2023-07-23 12:18:06 +0200

    update ProfileScreen

--- app/src/main/java/de/fhe/adoptapal/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/de/fhe/adoptapal/ui/screens/profile/ProfileScreen.kt
@@ -2,13 +2,14 @@ package de.fhe.adoptapal.ui.screens.profile
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -19,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.fhe.adoptapal.R
@@ -35,7 +35,7 @@ fun ProfileScreen(vm: ProfileScreenViewModel, modifier: Modifier = Modifier) {
     val user = remember { vm.user }
     val animalList = remember { vm.animalList }
 
-    Column() {
+    Column {
         Text(
             text = vm.user.value?.name?.uppercase() ?: "Keine User!",
             modifier = Modifier
@@ -51,7 +51,6 @@ fun ProfileScreen(vm: ProfileScreenViewModel, modifier: Modifier = Modifier) {
         LazyColumn(
             modifier = modifier
                 .fillMaxSize()
-                .background(color = colorResource(id = R.color.white))
         ) {
 
             if (user.value != null) {
@@ -65,37 +64,15 @@ fun ProfileScreen(vm: ProfileScreenViewModel, modifier: Modifier = Modifier) {
                             RatingBar(rating = 4.0)
                         }
 
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Text(
-                            text = "about ${vm.user.value!!.name}: Bio ist in DB nicht vorhanden",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                            color = colorResource(id = R.color.black),
-                            style = MaterialTheme.typography.overline,
-                            textAlign = TextAlign.Start,
-                            fontSize = 16.sp
-                        )
                         Spacer(modifier = modifier.height(24.dp))
                         Text(
-                            text = "Email:",
+                            text = "Email: ${vm.user.value!!.email}",
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(16.dp, 0.dp, 16.dp, 0.dp),
                             color = colorResource(id = R.color.black),
                             style = MaterialTheme.typography.overline,
                             fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Start,
-                            fontSize = 16.sp
-                        )
-                        Text(
-                            text = vm.user.value!!.email,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp, 0.dp, 16.dp, 0.dp),
-                            color = colorResource(id = R.color.black),
-                            style = MaterialTheme.typography.overline,
                             textAlign = TextAlign.Start,
                             fontSize = 16.sp
                         )
@@ -111,30 +88,21 @@ fun ProfileScreen(vm: ProfileScreenViewModel, modifier: Modifier = Modifier) {
                             textAlign = TextAlign.Start,
                             fontSize = 16.sp
                         )
+                        Spacer(modifier = modifier.height(24.dp))
                     }
                 }
             }
 
-        if (animalList.value.isNotEmpty()) {
-            
-            Text(text = "Gemerkte Tiere",
-                modifier = Modifier.padding(16.dp, 0.dp, 12.dp, 0.dp),
-                color = colorResource(id = R.color.black),
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.h5,
-                fontSize = 20.sp)
-            
-            AnimalList(
-                animals = animalList.value,
-                modifier = modifier
-            ) {
-                vm.navigateToAnimal(it)
-            }
-        } else {
-            FullscreenPlaceholderView("No Animals", Icons.Filled.Info)
             if (animalList.value.isNotEmpty()) {
                 item {
-                    Text(text = "Gemerkte Tiere!")
+                    Text(
+                        text = "Gemerkte Tiere",
+                        modifier = Modifier.padding(16.dp, 0.dp, 12.dp, 0.dp),
+                        color = colorResource(id = R.color.black),
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.h5,
+                        fontSize = 20.sp
+                    )
                 }
                 items(
                     items = animalList.value,
--- app/src/main/java/de/fhe/adoptapal/ui/screens/userDetail/UserDetailScreen.kt
+++ app/src/main/java/de/fhe/adoptapal/ui/screens/userDetail/UserDetailScreen.kt
@@ -1,58 +1,175 @@
 package de.fhe.adoptapal.ui.screens.userDetail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.fhe.adoptapal.R
+import de.fhe.adoptapal.ui.screens.home.AnimalCard
 import de.fhe.adoptapal.ui.screens.sharedComponents.AnimalList
+import de.fhe.adoptapal.ui.screens.sharedComponents.composeCall
+import de.fhe.adoptapal.ui.screens.sharedComponents.composeEmail
 import de.fhe.adoptapal.ui.screens.util.FullscreenPlaceholderView
 
 @Composable
 fun UserDetailScreen(vm: UserDetailScreenViewModel, modifier: Modifier = Modifier) {
     val animalList = remember { vm.animalList }
+    val context = LocalContext.current
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp, 16.dp, 16.dp, 16.dp)
-    ) {
+    Column{
+        Text(
+            text = vm.user.value?.name?.uppercase() ?: "Keine User!",
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            color = colorResource(id = R.color.black),
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.h1,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center
+        )
 
-        if (vm.user.value != null) {
-            UserInfo(
-                user = vm.user.value!!,
-                modifier = modifier
-            )
-        }
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            if (vm.user.value != null) {
+                item {
+                    Column(modifier = modifier) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp, 0.dp, 0.dp, 0.dp)
+                        ) {
+                            RatingBar(rating = 4.0)
+                        }
 
-        if (animalList.value.isNotEmpty()) {
+                        Spacer(modifier = modifier.height(24.dp))
+                        Text(
+                            text = "Email: ${vm.user.value!!.email}",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp, 0.dp, 16.dp, 0.dp),
+                            color = colorResource(id = R.color.black),
+                            style = MaterialTheme.typography.overline,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Start,
+                            fontSize = 16.sp
+                        )
+                        Spacer(modifier = modifier.height(24.dp))
+                        Text(
+                            text = "Telefon: ${vm.user.value!!.phoneNumber ?: "N/A"}",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp, 0.dp, 16.dp, 0.dp),
+                            color = colorResource(id = R.color.black),
+                            style = MaterialTheme.typography.overline,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Start,
+                            fontSize = 16.sp
+                        )
 
-            Text(text = "Hochgeladene Tiere",
-                modifier = Modifier.padding(16.dp, 0.dp, 12.dp, 0.dp),
-                color = colorResource(id = R.color.black),
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.h5,
-                fontSize = 20.sp)
+                        // create intent row
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
 
-            AnimalList(
-                animals = animalList.value,
-                modifier = modifier
-            ) {
-                vm.navigateToAnimal(it)
+
+                            if (vm.user.value!!.phoneNumber != null) {
+                                Button(
+                                    onClick = {
+                                        vm.user.value!!.phoneNumber?.let { composeCall(context, it) }
+                                    },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .padding(8.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                ) {
+                                    Text(
+                                        text = "Anrufen",
+                                        fontSize = 16.sp,
+                                    )
+                                }
+                            }
+                            Button(
+                                onClick = {
+                                    composeEmail(
+                                        context,
+                                        vm.user.value!!.email,
+                                        "Adoption von...",
+                                        "Guten Tag, \nich möchte einem Ihrer Tiere ein neues Zuhause bei mir bieten.\nViele Grüße\n"
+                                    )
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                                    .padding(8.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                            ) {
+                                Text(
+                                    text = "Email",
+                                    fontSize = 16.sp,
+                                )
+                            }
+                        }
+                    }
+                }
+
+                if (animalList.value.isNotEmpty()) {
+                    item{
+                        Text(text = "Hochgeladene Tiere",
+                            modifier = Modifier.padding(16.dp, 0.dp, 12.dp, 0.dp),
+                            color = colorResource(id = R.color.black),
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.h5,
+                            fontSize = 20.sp)
+                    }
+                    items(
+                        items = animalList.value,
+                        key = { it.id }
+                    ) {
+                        AnimalCard(
+                            it,
+                            modifier = modifier,
+                        ) {
+                            vm.navigateToAnimal(it)
+                        }
+                    }
+                } else {
+                    item {
+                        FullscreenPlaceholderView("No Animals", Icons.Filled.Info)
+                    }
+                }
             }
-        } else {
-            FullscreenPlaceholderView("No Animals", Icons.Filled.Info)
         }
     }
 }
